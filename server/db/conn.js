b58c6953fdcfdd630c8b222fb20405aaa943f6be
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-06-01 20:06:40 -0700

    Server connection established

---
+++ server/db/conn.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+const DB = process.env.ATLAS_URI;
+const client = new MongoClient(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology,
+});
+
+var _db;
+
+module.exports = {
+    connectToServer: function (callback) {
+        client.connect(function(err, db){
+
+            if(db){
+                _db = db.db("empolyees");
+                console.log("Successfully connected to MongoDB.")
+            } 
+
+            return callback(err);
+        });
+    },
+};
