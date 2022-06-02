00c03aed18bcb5de1657fce18c67a02ffb2c9e6c
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-06-01 19:50:17 -0700

    Initial commit: React on client and server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config({ path: "./config/env"});
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+
+const db = require("./db/conn");
+
+app.listen(PORT, () => {
+
+    db.connectToServer(function(err){
+        if(err) console.error(err);
+
+    });
+    console.log(`Server is running on port: ${PORT}`)
+})
