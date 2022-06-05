a5d9b9deb9e16301ba948f48775845b7cb48439e
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-06-05 12:25:09 -0700

    Please work this time

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 app.use(express.static(`${__dirname}/../client/build`));
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
